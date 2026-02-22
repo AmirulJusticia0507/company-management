fe98fdcdde9de9f9423525b7d31753d89b8be6c0
amirulputra0507 <amirulputra0507@gmail.com>
2026-02-22 10:24:37 +0700

    update seed
    update seed

--- database/seeders/CompanyEmployeeSeeder.php
+++ database/seeders/CompanyEmployeeSeeder.php
@@ -11,24 +11,24 @@ class CompanyEmployeeSeeder extends Seeder
     public function run()
     {
         $company = Company::create([
-            'name' => 'Acme Corp',
-            'email' => 'acme@example.com',
+            'name' => 'CR7 Corp',
+            'email' => 'cr7siuuu@gmail.com',
             'logo' => '',
-            'website' => 'https://acme.com',
+            'website' => 'https://cr7.com',
         ]);
 
         Employee::create([
-            'first_name' => 'John',
-            'last_name' => 'Doe',
-            'email' => 'john@acme.com',
+            'first_name' => 'Cristiano',
+            'last_name' => 'Ronaldo',
+            'email' => 'cr7official@gmail.com',
             'phone' => '0812345678',
             'company_id' => $company->id,
         ]);
 
         Employee::create([
-            'first_name' => 'Jane',
-            'last_name' => 'Smith',
-            'email' => 'jane@acme.com',
+            'first_name' => 'Lionel Andres',
+            'last_name' => 'Messi',
+            'email' => 'leomessi10@gmail.com.com',
             'phone' => '08987654321',
             'company_id' => $company->id,
         ]);
